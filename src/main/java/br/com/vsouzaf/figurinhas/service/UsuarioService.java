9636e558ab8472de7388e05d7032be2462c479ad
Valtemir Souza <valtemir.souza168@gmail.com>
2018-09-02 23:04:09 -0300

    Implementação de abstrações

--- src/main/java/br/com/vsouzaf/figurinhas/service/UsuarioService.java
+++ src/main/java/br/com/vsouzaf/figurinhas/service/UsuarioService.java
@@ -1,40 +1,15 @@
 package br.com.vsouzaf.figurinhas.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import br.com.vsouzaf.figurinhas.entity.Usuario;
 import br.com.vsouzaf.figurinhas.repository.UsuarioRepository;
+import br.com.vsouzaf.figurinhas.to.UsuarioTo;
 
 @Service
-public class UsuarioService {
-	
-	@Autowired
-	UsuarioRepository usuarioRepository;
-	
-	public List<Usuario> listar() {
-        return usuarioRepository.findAll();
-    }
-	
-	public Page<Usuario> listarPaginado( int page, int count) {
-        Pageable pages = new PageRequest(page, count);
-        return usuarioRepository.findAll(pages);
-    }
+public class UsuarioService extends ServicoCrudAbstrata<UsuarioTo, Usuario, String>{
 
-    public Usuario salvar(Usuario usuarioAdd) {
-        return usuarioRepository.save(usuarioAdd);
-    }
-
-    public void deletar(String id) {
-        usuarioRepository.delete(id);
-    }
-
-    public Usuario getById(String id) {
-        return usuarioRepository.findOne(id);
-    }
+	public UsuarioService(UsuarioRepository repo) {
+		super(repo);
+	}
 }
